e6ec756ab7f73daa342167fdc5a22038533f6ffe
0nly1ken0bi <54916539+0nly1ken0bi@users.noreply.github.com>
2026-02-06 15:48:06 +0200

    Delete phase0 directory

--- phase0/i5_occlusion_sanity.py
+++
@@ -1,8 +0,0 @@
-def check_occlusion_sanity(face: dict):
-    if 500 in face:
-        raise ValueError("Eye occlusion")
-
-    if 501 in face:
-        raise ValueError("Mouth occlusion")
-
-    return True
