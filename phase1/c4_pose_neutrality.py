cecb4e6cfc15c20b5934295d6649ac390d6d58bc
0nly1ken0bi <54916539+0nly1ken0bi@users.noreply.github.com>
2026-02-10 07:00:45 +0200

    Delete phase1 directory

--- phase1/c4_pose_neutrality.py
+++
@@ -1,3 +0,0 @@
-def validate_pose_neutrality(landmarks):
-    # Phase 1 pose neutrality is satisfied if head warp does not raise
-    return True
--- phase1/phase1_overlay.py
+++
@@ -1,2 +0,0 @@
-def draw_phase1_failure_overlay(img, face, message):
-    return img
